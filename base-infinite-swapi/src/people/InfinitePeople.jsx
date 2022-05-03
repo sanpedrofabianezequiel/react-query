6d0656fc3b55e429e9aa47c532cf40873435b0f4
Fabian Ezequiel San Pedro <fabian.sanpedro@globant.com>
2022-05-03 00:58:16 -0300

    Creat infinite scroll

--- base-infinite-swapi/src/people/InfinitePeople.jsx
+++ base-infinite-swapi/src/people/InfinitePeople.jsx
@@ -1,5 +1,6 @@
 import InfiniteScroll from "react-infinite-scroller";
 import { Person } from "./Person";
+import { useInfiniteQuery } from "react-query";
 
 const initialUrl = "https://swapi.dev/api/people/";
 const fetchUrl = async (url) => {
@@ -8,6 +9,21 @@ const fetchUrl = async (url) => {
 };
 
 export function InfinitePeople() {
-  // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  const { data, hasNextPage, fetchNextPage, isLoading, isError, error, isFetching } = useInfiniteQuery("sw-people", ({ pageParam = initialUrl }) => fetchUrl(pageParam), {
+    getNextPageParam: (lastGroup) => lastGroup.next || undefined,
+  });
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p>Error: {error.message}</p>;
+  return (
+    <>
+      {isFetching && <div className="loading">Loading...</div>}
+      <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage}>
+        {data.pages.map((x) => {
+          return x.results.map((y) => {
+            return <Person key={y.name} name={y.name} hairColor={y.hairColor} eyeColor={y.eyeColor} />;
+          });
+        })}
+      </InfiniteScroll>
+    </>
+  );
 }
--- base-infinite-swapi/src/species/InfiniteSpecies.jsx
+++ base-infinite-swapi/src/species/InfiniteSpecies.jsx
@@ -1,5 +1,6 @@
 import InfiniteScroll from "react-infinite-scroller";
 import { Species } from "./Species";
+import { useInfiniteQuery } from "react-query";
 
 const initialUrl = "https://swapi.dev/api/species/";
 const fetchUrl = async (url) => {
@@ -8,6 +9,25 @@ const fetchUrl = async (url) => {
 };
 
 export function InfiniteSpecies() {
-  // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  const { data, hasNextPage, fetchNextPage, isLoading, isError, error, isFetching } = useInfiniteQuery("sw-species", ({ pageParam = initialUrl }) => fetchUrl(pageParam), {
+    getNextPageParam: (lastGroup) => lastGroup.next || undefined,
+  })
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p>Error: {error.message}</p>;
+
+  return(
+    <>
+      {isFetching && <div className="loading">Loading...</div>}
+      <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage}>
+        {
+          data.pages.map((x) => {
+            return x.results.map((y)=>{
+              return <Species key={y.name} name={y.name} language={y.language} averageLifespan={y.averageLifespan} />
+            })
+          })
+        }
+      </InfiniteScroll>
+    </>
+  ); 
+
 }
